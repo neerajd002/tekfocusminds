2600adb13263b7937ff71ae4a26b63c168ab433c
Neeraj Deshmukh <neerajd002@gmail.com>
2024-05-06 22:42:51 +0530

    Careers Page Update

--- js/data.js
+++ js/data.js
@@ -1,75 +1,26 @@
 const jobs = [
     {
-        role: `Java Developer`,
-        description: `
-            - Design, develop, and deploy microservices-based applications using Java and Spring Boot. 
-            - Implement RESTful APIs, event-driven architectures, and asynchronous communication patterns.
-            <br>
-            <br>
-            **Must-Have Skills:** 
-            1. Java and Spring Boot:
-                - Strong proficiency in Java programming. 
-                - Experience with Spring Boot for building RESTful services.
-            2. Microservices Architecture: 
-                - In-depth understanding of microservices principles and patterns. 
-                - Familiarity with service discovery, load balancing, and circuit breakers. 
-            3. Database Expertise: 
-                - Proficiency in SQL and NoSQL databases. 
-                - Ability to design efficient database schemas and optimize queries.
-        `,
+        title: 'Java Developer',
+        description: 'Design, develop, and deploy microservices-based applications using Java and Spring Boot. Implement RESTful APIs, event-driven architectures, and asynchronous communication patterns.',
         experience: `7 Years`,
-        relevant_experience: `6 Years`,
-        budget: `12-15 LPA`,
-        location: `Banglore`,
-        no_of_openings: 4,
-        notice_period: `1 Month`
+        country: 'India',
+        location: 'Bengaluru',
+        download: "./downloads/JD.pdf"
     },
     {
-        role: `Dot Net Developer`,
-        description: `
-            - Design, develop and implement business and technical features in batch processes and web application. 
-            Critical Skills to Possess: 
-            - .Net MVC/Core, C#, Javascript and sql server, ASP.net 
-            - 6+ years of experience with object-oriented application development in a Microsoft .NET environment. 
-            - 6+ years of experience with SQL Server. 
-            - 5+ years of experience with web technologies. 
-            - 5+ experience with windows services / microservices / APIs. 
-            - Experience working in projects using Agile methodology.     
-        `,
+        title: 'Dot Net Developer',
+        description: 'Design, develop and implement business and technical features in batch processes and web application. .Net MVC/Core, C#, Javascript and sql server, ASP.net ',
         experience: `5+ Years`,
-        relevant_experience: `4-5 Years`,
-        budget: `18 LPA`,
-        location: `Banglore`,
-        no_of_openings: 2,
-        notice_period: `1 Month`
+        country: 'India',
+        location: 'Bengaluru',
+        download: "./downloads/JD.pdf"
     },
     {
-        role: `Technical Recruiter`,
-        description: `
-            - We are seeking a talented Technical Recruiter to join our team in the computer software industry. As a Technical Recruiter, you will be responsible for identifying, sourcing, and hiring top technical talent to support the functional growth of our organization.
-            - Your mission will be to help us identify and attract the best candidates. You will play a critical role in building a culture of excellence, innovation, and success through your recruitment efforts. You’ll work closely with hiring managers to understand their needs and ensure that every candidate we bring on board is an ideal fit for both the role and our company culture.
-            <br>
-            <br>
-            
-            **Responsibilities**
-            - Develop and implement effective sourcing strategies to attract top talent
-            - Utilize social media, resume databases, and any other sources, to identify candidates
-            - Screen and qualify candidates using a range of interview methods
-            - Schedule and coordinate interviews with hiring managers
-            - Continuously monitor and improve the recruitment process to ensure effectiveness and efficiency
-            <br>
-            <br>
-
-            **Requirements**
-            - Proven experience of minimum 6 months as a technical recruiter in the computer software Industry
-            - Knowledge upon different hiring portal like Naukri, Indeed, Hirist, etc.
-            - Strong knowledge of recruiting principles and best practices
-            - Excellent communication skills, written and verbal
-            - Ability to build positive relationships with colleagues and candidates
-            - Strong time management skills with ability to organize and prioritize workload
-            - Ability to work independently and in a team environment
-            - Bachelor’s degree in Human Resources.
-
-        `,
+        title: 'Technical Recruiter',
+        description: 'We are seeking a talented Technical Recruiter to join our team in the computer software industry. As a Technical Recruiter, you will be responsible for identifying, sourcing, and hiring top technical talent to support the functional growth of our organization. Your mission will be to help us identify and attract the best candidates. You will play a critical role in building a culture of excellence, innovation, and success through your recruitment efforts. You’ll work closely with hiring managers to understand their needs and ensure that every candidate we bring on board is an ideal fit for both the role and our company culture.',
+        experience: '5+ Years',
+        country: 'India',
+        location: 'Bengaluru',
+        download: "./downloads/JD_Technical_Recruiter.pdf"
     }
 ]
